48d6f0fe6c1bdef641477b0f18177676f9cb132b
melissalau17 <melissalau17300@gmail.com>
2025-08-17 01:12:09 +0700

    a

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,11 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, 
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   env: {
-    NEXT_PUBLIC_API_URL: 'https://si-desa2.onrender.com',
+    NEXT_PUBLIC_API_URL: "https://si-desa2.onrender.com",
   },
 };
 
