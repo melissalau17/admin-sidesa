481a3382bad55bef2b28e11b96268761f2c6ce42
melissalau17 <melissalau17300@gmail.com>
2025-08-17 01:00:31 +0700

    added eslint ignoreDuringBuilds

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, 
+  },
   env: {
     NEXT_PUBLIC_API_URL: 'https://si-desa2.onrender.com',
   },
