e6b819ca699765510afed6c1ca1749a98388bb45
melissalau17 <melissalau17300@gmail.com>
2025-09-01 01:43:09 +0700

    fixed print surat feature

--- src/app/(dashboard)/kelola-surat/print/[id]/page.tsx
+++ src/app/(dashboard)/kelola-surat/print/[id]/page.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from 'react';
 import { notFound } from 'next/navigation';
 import axios from 'axios';
 
-export default function SuratView({ params }: { params: { id: string } }) {
-    const { id } = params;
+export default function SuratView(props: { params: { id: string } }) {
+    const { id } = props.params;
     const [pdfUrl, setPdfUrl] = useState<string | null>(null);
 
     useEffect(() => {
